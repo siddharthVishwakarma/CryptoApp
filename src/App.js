829058192d27b22242cf0024f2fb453d6e2688bc
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-03-05 23:57:32 +0530

    initial navbar setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main"></div>
+        <div className="footer"></div>
+      </div>
+    </>
+  );
 };
 
 export default App;
