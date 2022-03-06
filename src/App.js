3dd3f1ff19b5d83cb52f040f5ebe26eb18319275
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-03-06 14:28:54 +0530

    footer, main setup

--- src/App.js
+++ src/App.js
@@ -9,39 +9,46 @@ import {
   News,
 } from "./components";
 import "./App.css";
-import { Layout } from "antd";
+import { Layout, Space, Typography } from "antd";
 
 const App = () => {
   return (
-    <>
-      <div className="app">
-        <div className="navbar">
-          <Navbar />
-        </div>
-        <div className="main">
-          <Layout>
-            <div className="routs">
-              <Routes>
-                <Route exact path="/" element={<HomePage />} />
-                <Route exact path="/exchanges" element={<Exchanges />} />
-                <Route
-                  exact
-                  path="/cryptocurrencies"
-                  element={<Cryptocurrencies />}
-                />
-                <Route
-                  exact
-                  path="/crypto:coinId"
-                  element={<CryptoDetails />}
-                />
-                <Route exact path="/news" element={<News />} />
-              </Routes>
-            </div>
-          </Layout>
-        </div>
-        <div className="footer"></div>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
       </div>
-    </>
+      <div className="main">
+        <Layout>
+          <div className="routs">
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All Rights Reserved
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+          </Typography.Title>
+        </div>
+      </div>
+    </div>
   );
 };
 
